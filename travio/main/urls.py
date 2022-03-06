ad7f62a02d8abb462dd19aaf5b004aca9dbc6ea7
Dhruvi Sherathiya <83078196+DhruviSherathiya@users.noreply.github.com>
2022-03-07 00:19:26 +0530

    Login Page

--- travio/main/urls.py
+++ travio/main/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('register',views.register,name="register")
 ]
--- travio/main/views.py
+++ travio/main/views.py
@@ -6,3 +6,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+def register(request):
+    return render(request,"register.html")
