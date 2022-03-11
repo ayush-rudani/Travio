5b7087d9bd4ac5d93ae0e2ef507f8a9a6d666de9
Ayush <ayushcrudani@gmail.com>
2022-03-11 16:21:20 +0530

    Added CSS.html and JS.html for CSS & JS links

--- travio/travio/urls.py
+++ travio/travio/urls.py
@@ -13,13 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
+
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
+# urlpatterns += staticfiles_urlpatterns()
+# urlpatterns += static(settings.STATIC_URL, document_root.settings.STATIC_ROOT)
