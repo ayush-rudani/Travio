d7ab213d2ecede490bc77b89166d3de0cef54960
Dhruvi Sherathiya <83078196+DhruviSherathiya@users.noreply.github.com>
2022-04-08 01:42:36 +0530

    Final

--- travio/main/views.py
+++ travio/main/views.py
@@ -255,7 +255,7 @@ def profile(request):
 
 def tHistory(request):
     user = request.user
-    history = Booking.object.all().filter(uid=user.id)
+    history = Booking.objects.all().filter(uid=user.id)
     return render(request, "tourhistory.html",{'history': history})
 
 # class Counter:
