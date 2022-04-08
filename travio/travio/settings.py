4acd76f742c43984f210a52eb1badcd47eee5b31
Ayush Rudani <ayushcrudani@gmail.com>
2022-04-08 12:48:50 +0530

    Update

--- travio/travio/settings.py
+++ travio/travio/settings.py
@@ -61,7 +61,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-            ],
+            ], 'libraries':{
+                'total_cost': 'main.templatetags.rec',
+            }
         },
     },
 ]
